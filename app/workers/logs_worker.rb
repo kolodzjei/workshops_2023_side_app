ac91e11ce0ed3eaf2dbf0203890bee724936c969
Dawid Kolodziej <kolodziejdawid02@gmail.com>
2023-05-26 23:45:45 +0200

    Update worker to consume cancel_loans and add routing key to log's data

--- app/workers/logs_worker.rb
+++ app/workers/logs_worker.rb
@@ -1,13 +1,18 @@
 class LogsWorker
   include Sneakers::Worker
 
-  from_queue 'basic_app_book_logs', exchange: 'basic_app', exchange_type: :direct, routing_key: 'basic_app.book_loans'
+  from_queue 'basic_app_book_logs', exchange: 'basic_app', exchange_type: :direct, routing_key: ['basic_app.book_loans', 'basic_app.cancel_loans']
 
-  def work(msg)
-    Log.create(log_attributes(JSON.parse(msg)))
+  def work_with_params(msg, delivery_info, metadata)
+    Log.create(log_attributes(setup_message(msg, delivery_info)))
   end
 
   private
+
+  def setup_message(msg, delivery_info)
+    msg = JSON.parse(msg)
+    msg.merge({ routing_key: delivery_info.routing_key })
+  end
 
   def log_attributes(message)
     {
