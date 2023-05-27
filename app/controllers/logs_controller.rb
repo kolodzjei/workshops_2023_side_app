2552e95343c9665e37de840a5a7523d36139d3c3
Dawid Kolodziej <kolodziejdawid02@gmail.com>
2023-05-27 17:13:31 +0200

    Add json response to logs controller, add scope for user id to log model

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,10 @@
 class LogsController < ApplicationController
   def index
-    @logs = Log.all
+    @logs = params[:user_id].present? ? Log.for_user_id(params[:user_id]) : Log.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @logs }
+    end
   end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -2,6 +2,8 @@
 
 class Log < ApplicationRecord
   include ActiveModel::Serializers::JSON
+
+  scope :for_user_id, ->(user_id) { where(user_id: user_id) }
   
   validates :user_id, presence: true
   validates :data, presence: true
